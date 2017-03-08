92bfb4a8f8ecee7f1df7d90f2b2b2ddbf82c8db0
Ludwigo <Ludwigo@DESKTOP-NPNAKK5>
2017-03-07 23:27:38 -0500

    nothing new

---
+++ casinobot/src/casinobot/cactpot.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+/**
+ * @author Ludwigo
+ *
+ */
+package casinobot;
+
+import javax.security.auth.login.LoginException;
+import java.util.List;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.*;
+
+public class cactpot extends ListenerAdapter {
+	
+	public static void main(String[] args) {
+		try {
+			new JDABuilder(AccountType.BOT)
+			.setToken("Mjg2OTA3NjU1MjM3MjcxNTUy.C5nlJA.8SYQDkro428pmEFZrtI4znyZJkU")
+			.addListener(new cactpot())
+			.buildAsync();
+		} catch (LoginException | IllegalArgumentException | RateLimitedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // Build JDA - connect to discord
+	}
+		@Override	
+		public void onMessageReceived(MessageReceivedEvent event) {
+			JDA jda = event.getJDA();
+			long reponseNumber = event.getResponseNumber();
+		}
+}
